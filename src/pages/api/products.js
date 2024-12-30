8bc7341d65fe1dbe929e58896d1bba865d5d156c
ju78832 <junedgreat89@gmail.com>
2024-12-30 21:24:30 +0530

    Update the changes

--- src/pages/api/products.js
+++ src/pages/api/products.js
@@ -5,7 +5,13 @@ export async function findAllProducts() {
   return Product.find().exec();
 }
 
-export default async function handle(req, res) {
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
+export async function GET(req, res) {
   await initMongoose();
   const { ids } = req.query;
   if (ids) {
@@ -17,5 +23,45 @@ export default async function handle(req, res) {
     );
   } else {
     res.json(await findAllProducts());
+  }
+}
+
+export async function POST(req, res) {
+  await initMongoose();
+  try {
+    const formData = await req.formData();
+    const imageFile = formData.get("image");
+    const name = formData.get("name");
+    const description = formData.get("description");
+    const price = formData.get("price");
+    const category = formData.get("category");
+
+    const byte = await imageFile.arrayBuffer();
+    const buffer = Buffer.from(byte);
+
+    const result = await new Promise((resolve, reject) =>
+      cloudinary.uploader.upload(
+        buffer,
+        { folder: "product" },
+        (error, result) => {
+          if (error) {
+            reject(error);
+          } else {
+            resolve(result);
+          }
+        }
+      )
+    );
+
+    const product = await Product.create({
+      name,
+      description,
+      price,
+      category,
+      picture: result.secure_url,
+    });
+    res.json(product);
+  } catch (error) {
+    res.json({ message: "Something went wrong" }, { status: 500 });
   }
 }
